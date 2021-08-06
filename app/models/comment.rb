59d481406b40f2f34043c2a4257738b2000d8312
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-08-06 04:14:22 +0200

    hotwire

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,5 +17,13 @@
 #  fk_rails_...  (project_id => projects.id)
 #
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :project
+
+  after_create_commit {
+    broadcast_prepend_to [project, :comments],
+                         target: "#{dom_id(project)}_comments"
+  }
+
+  validates :body, presence: true
 end
