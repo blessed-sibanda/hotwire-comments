a87a5c7c02822fa359cca3f09ae29fa235176d2f
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-08-06 04:38:13 +0200

    root to projects#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :comments, only: :create
   end
+  root to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
